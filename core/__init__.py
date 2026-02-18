9e059a2141f0ce1c6175146fbfd668e5357dc9a3
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-02-18 22:31:45 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,24 +1,26 @@
 """
-Core modules for ARUNABHA EXTREME FEAR BOT v2.0
+Core modules for ARUNABHA EXTREME FEAR BOT v3.0
 """
 
-from .extreme_fear_engine import ExtremeFearEngine
+from .extreme_fear_engine import ExtremeFearEngine, extreme_fear_engine
 from .simple_filters import SimpleFilters
-from .risk_manager import RiskManager, Trade
+from .risk_manager import RiskManager, Trade, risk_manager  # 🆕 Export instance too
 from .market_mood import MarketMood, get_fear_index
 from .smart_signal import generate_signal, SignalResult
 from .btc_regime_detector import BTCRegimeDetector, BTCRegime, btc_detector
 
 __all__ = [
     "ExtremeFearEngine",
+    "extreme_fear_engine",
     "SimpleFilters",
     "RiskManager",
     "Trade",
+    "risk_manager",  # 🆕 Instance
     "MarketMood",
     "get_fear_index",
     "generate_signal",
     "SignalResult",
-    "BTCRegimeDetector",  # 🆕 New
-    "BTCRegime",          # 🆕 New
-    "btc_detector",       # 🆕 New
+    "BTCRegimeDetector",
+    "BTCRegime",
+    "btc_detector",
 ]
