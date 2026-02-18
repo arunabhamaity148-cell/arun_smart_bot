1abf7c12c7d5bd8a05fa3f43e1e309b3d9a53980
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-02-18 16:19:42 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,21 @@
-from .time_utils import utcnow, ist_now, is_sleep_time, today_ist_str, ts_label
+"""
+Utility modules for ARUNABHA EXTREME FEAR BOT
+"""
 
-__all__ = ["utcnow", "ist_now", "is_sleep_time", "today_ist_str", "ts_label"]
+from .time_utils import (
+    utcnow,
+    ist_now,
+    is_sleep_time,
+    today_ist_str,
+    ts_label,
+    format_duration
+)
+
+__all__ = [
+    "utcnow",
+    "ist_now",
+    "is_sleep_time",
+    "today_ist_str",
+    "ts_label",
+    "format_duration",
+]
