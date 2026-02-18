883423a9835e9146b9af7387b9e3d9fe1b6d0406
arunabhamaity148-cell <arunabhamaity148@gmail.com>
2026-02-18 20:23:26 +0530

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,5 @@
 """
-Core modules for ARUNABHA EXTREME FEAR BOT v1.0
+Core modules for ARUNABHA EXTREME FEAR BOT v2.0
 """
 
 from .extreme_fear_engine import ExtremeFearEngine
@@ -7,6 +7,7 @@ from .simple_filters import SimpleFilters
 from .risk_manager import RiskManager, Trade
 from .market_mood import MarketMood, get_fear_index
 from .smart_signal import generate_signal, SignalResult
+from .btc_regime_detector import BTCRegimeDetector, BTCRegime, btc_detector
 
 __all__ = [
     "ExtremeFearEngine",
@@ -17,4 +18,7 @@ __all__ = [
     "get_fear_index",
     "generate_signal",
     "SignalResult",
+    "BTCRegimeDetector",  # 🆕 New
+    "BTCRegime",          # 🆕 New
+    "btc_detector",       # 🆕 New
 ]
